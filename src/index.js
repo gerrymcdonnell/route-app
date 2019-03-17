2ebaf8e15031d4384352003d0e8a8fba51c96a79
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-17 16:34:53 +0000

    import path fix

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-import One from '.components/One';
-import Two from '.components/Two';
-import Three from '.components/Three';
-import Four from '.components/Four';
-import NoMatch from '.components/NoMatch';
-import Fourpointone from '.components/Fourpointone';
+import One from './components/One';
+import Two from './components/Two';
+import Three from './components/Three';
+import Four from './components/Four';
+import NoMatch from './components/NoMatch';
+import Fourpointone from './components/Fourpointone';
 
 
 import { Router, Route, browserHistory } from 'react-router'
